9058d965bab741054b2b4e847e2dc5e4955f0701
Pramit Sawant <pramitsawant11@gmail.com>
2021-11-16 12:42:13 +0530

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" sm="8" md="6">
       <v-card>
         <v-card-title class="headline">
-          Welcome to {{ $config.instanceName }}
+          Welcome to - {{ $config.instanceName }}
         </v-card-title>
       </v-card>
     </v-col>
